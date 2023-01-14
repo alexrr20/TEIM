408d380606007931b18d7274d19056152a853870
Pedro Baldaia <pmbaldaia@Baldaia.Home>
2023-01-14 15:43:31 +0000

    BugsLogo

---
+++ src/Screens/Home.js
@@ -0,0 +1,32 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {DateTime} from 'luxon';
+import {useTheme} from '@react-navigation/native';
+
+export default function Home() {
+  const now = DateTime.now().setLocale('pt').toLocaleString(DateTime.DATE_HUGE);
+
+  const {colors} = useTheme();
+
+  const styles = StyleSheet.create({
+    pageDate: {
+      color: colors.text,
+      fontFamily: 'PPNeueMontreal-Medium',
+      fontSize: 14.22,
+    },
+    pageHeader: {
+      color: colors.text,
+      fontFamily: 'PPNeueMontreal-Bold',
+      fontSize: 32.44,
+    },
+  });
+
+  return (
+    <View>
+      <View>
+        <Text style={styles.pageDate}>{now}</Text>
+        <Text style={styles.pageHeader}>Página Inicial</Text>
+      </View>
+    </View>
+  );
+}
