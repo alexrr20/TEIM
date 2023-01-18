8337848ddf3eec0d9d62b2137fc5a7e50d0335ab
alexrr20 <alex11.bessa@gmail.com>
2023-01-18 00:44:19 +0000

    logout button

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -46,11 +46,13 @@ const Task = props => {
     DurationContainer: {
       display: 'flex',
       flexDirection: 'row',
+      alignItems: 'center',
     },
     DurationText: {
       fontFamily: 'PPNeueMontreal-Medium',
-      fontSize: 14.22,
+      fontSize: 16,
       color: colors.text,
+      marginHorizontal: 4,
     },
     deleteBox: {
       marginTop: 5,
@@ -105,8 +107,8 @@ const Task = props => {
           <View style={styles.DurationContainer}>
             <Text style={styles.DurationText}>14h</Text>
             <Svg
-              width="21"
-              height="21"
+              width="25"
+              height="31"
               viewBox="0 0 15 15"
               fill="none"
               xmlns="http://www.w3.org/2000/svg">
--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,10 +1,13 @@
 import {View, Text, StyleSheet, Pressable} from 'react-native';
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Header from '../Components/Header';
 import {Path, Rect, Svg, Line} from 'react-native-svg';
 import {useTheme} from '@react-navigation/native';
+import {AuthContext} from '../Context/AuthContext';
 
 const Profile = ({navigation}) => {
+  const {logout} = useContext(AuthContext);
+
   const {colors} = useTheme();
 
   const [isAdmin, setisAdmin] = useState(true);
@@ -154,11 +157,30 @@ const Profile = ({navigation}) => {
       color: colors.text,
       fontSize: 14,
     },
+    logoutContainer: {
+      position: 'absolute',
+      right: 20,
+    },
   });
 
   return (
     <View>
       <Header headerText={'Perfil'} />
+      <Pressable onPress={logout} style={styles.logoutContainer}>
+        <Svg
+          width="20"
+          height="20"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <Path
+            d="M3 1C2.44771 1 2 1.44772 2 2V13C2 13.5523 2.44772 14 3 14H10.5C10.7761 14 11 13.7761 11 13.5C11 13.2239 10.7761 13 10.5 13H3V2L10.5 2C10.7761 2 11 1.77614 11 1.5C11 1.22386 10.7761 1 10.5 1H3ZM12.6036 4.89645C12.4083 4.70118 12.0917 4.70118 11.8964 4.89645C11.7012 5.09171 11.7012 5.40829 11.8964 5.60355L13.2929 7H6.5C6.22386 7 6 7.22386 6 7.5C6 7.77614 6.22386 8 6.5 8H13.2929L11.8964 9.39645C11.7012 9.59171 11.7012 9.90829 11.8964 10.1036C12.0917 10.2988 12.4083 10.2988 12.6036 10.1036L14.8536 7.85355C15.0488 7.65829 15.0488 7.34171 14.8536 7.14645L12.6036 4.89645Z"
+            fill="#faf2ec"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          />
+        </Svg>
+      </Pressable>
       {renderAdmin()}
     </View>
   );
