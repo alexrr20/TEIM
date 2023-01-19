e6a940682187d0df0ebf2367308cc2f3823a2c02
Pedro Baldaia <pmbaldaia@Baldaia.local>
2023-01-19 12:07:06 +0000

    UpdateEyeLogin

--- src/Components/TextInputLogin.js
+++ src/Components/TextInputLogin.js
@@ -66,28 +66,29 @@ function textInput1({control, name}) {
           control={control}
           name={name[1]}
           render={({field: {value, onChange}}) => (
-            <TextInput
-              value={value}
-              onTextChange={onChange}
-              autoCorrect={false}
-              secureTextEntry={passwordVisibility}
-            />
+            <Pressable onPress={handlePasswordVisibility}>
+              <TextInput
+                value={value}
+                onTextChange={onChange}
+                autoCorrect={false}
+                secureTextEntry={passwordVisibility}
+              />
+              <Svg
+                viewBox="0 0 100 100"
+                width="130"
+                height="130"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{right: -90, position: 'absolute', bottom: -100}}>
+                <Path
+                  d="M7 15.5L5.5 17.5M20.5 12.5C19.8612 13.5647 19.041 14.6294 18.0008 15.501M18.0008 15.501C16.5985 16.676 14.7965 17.5 12.5 17.5M18.0008 15.501L18 15.5M18.0008 15.501L19.5 17.5M12.5 17.5C8.5 17.5 6 15 4.5 12.5M12.5 17.5V20M15.5 17L16.5 19.5M9.5 17L8.5 19.5"
+                  stroke="#121923"
+                  stroke-width="1.2"
+                />
+              </Svg>
+            </Pressable>
           )}
         />
-        <Pressable onPress={handlePasswordVisibility}>
-          <Svg
-            viewBox="0 0 100 100"
-            width="100"
-            height="100"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <Path
-              d="M7 15.5L5.5 17.5M20.5 12.5C19.8612 13.5647 19.041 14.6294 18.0008 15.501M18.0008 15.501C16.5985 16.676 14.7965 17.5 12.5 17.5M18.0008 15.501L18 15.5M18.0008 15.501L19.5 17.5M12.5 17.5C8.5 17.5 6 15 4.5 12.5M12.5 17.5V20M15.5 17L16.5 19.5M9.5 17L8.5 19.5"
-              stroke="white" /* "#121923" */
-              stroke-width="1.2"
-            />
-          </Svg>
-        </Pressable>
       </View>
     </View>
   );
